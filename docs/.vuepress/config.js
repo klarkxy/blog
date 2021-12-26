7bd8fe68a5878b74ff8abcc856024b4904ffd59c
klarkxy <278370456@qq.com>
2021-12-26 14:14:48 +0800

    增加vssue评论插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,27 @@
 module.exports = {
-    // 站点配置
-    lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
-  
-    // 主题和它的配置
-    theme: '@vuepress/theme-default',
-    themeConfig: {
-      logo: 'https://vuejs.org/images/logo.png',
-    },
-  }
+  // 站点配置
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  // 插件
+  plugins: [
+    [
+      '@vuepress/plugin-search',
+      {
+        placeholder: '搜索',
+      },
+    ],
+    ['@vssue/vuepress-plugin-vssue', {
+      platform: 'github',
+      owner: 'klarkxy',
+      repo: 'klarkxy.github.io',
+      clientId: 'af5542c439c6b39a9a26',
+      clientSecret: '49e7076253861c5e0729809cf3fc34587b76918f',
+    }],
+  ],
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+}
