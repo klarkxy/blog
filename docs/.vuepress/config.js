6f4e8854ff843741995de06824841529ce1ec51e
klarkxy <278370456@qq.com>
2021-12-26 12:03:33 +0800

    1226

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,12 @@
 module.exports = {
+    // 站点配置
     lang: 'zh-CN',
     title: '你好， VuePress ！',
     description: '这是我的第一个 VuePress 站点',
-
+  
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
     themeConfig: {
-        nav: [//导航栏
-            { text: '主页', link: '/' },
-            { text: '目录',link:'/catalog'}
-        ],
-        catalogUrl:'/catalog',//必填 目录路径
-        lastUpdated: 'Last Updated',//必填：文章显示最新修改时间
+      logo: 'https://vuejs.org/images/logo.png',
     },
-    
-    theme: 'ting'
-}
+  }
